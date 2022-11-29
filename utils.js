9bb354119ee1429a9fdab84f225bd43f156a7c21
Ines-lmahdi <101194731+ines-lmahdi@users.noreply.github.com>
2022-11-29 11:57:04 +0100

    Ajout fichiers json et js

---
+++ utils.js
@@ -0,0 +1,31 @@
+function openForm(){
+    document.getElementById("popupForm").style.display ="block";
+}
+function closeForm(){
+    document.getElementById("popupForm").style.display ="none";
+}
+
+/*
+LOGO ECOLE
+
+<i class="fa-solid fa-graduation-cap"></i>
+
+
+LOGO ORDINATEUR
+
+<i class="fa-solid fa-chalkboard-user"></i>
+<i class="fa-solid fa-laptop-code"></i>
+<i class="fa-solid fa-laptop"></i>
+
+LOGO VOYAGE
+
+<i class="fa-solid fa-plane-departure"></i>
+<i class="fa-solid fa-earth-americas"></i>
+<i class="fa-solid fa-suitcase"></i>
+
+LOGO SPORT
+
+<i class="fa-solid fa-broom-ball"></i>
+<i class="fa-solid fa-broom-ball"></i>
+*/
+
